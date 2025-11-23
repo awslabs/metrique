3121297179da5a2a9a05adfb4b9dadfd6f3ccbb1
Ariel Ben-Yehuda <arielby@amazon.com>
2025-11-23 12:42:50 +0000

    feat: add support for Tokio 1.38, test MSRV
    This has been hand-tested to work. We do not plan to have CI testing for
    minimal versions, use with old versions of libraries is at customers'
    risk.
    
    Also documented MSRV policy and update guide.

--- metrique/tests/cargo-toml-format.rs
+++ metrique/tests/cargo-toml-format.rs
@@ -2,6 +2,13 @@ use rstest::rstest;
 use std::fs;
 use std::path::PathBuf;
 
+const MSRV: &'static str = "1.87.0";
+
+// return just major and minor versions of msrv
+fn msrv_major_minor() -> String {
+    MSRV.split('.').take(2).collect::<Vec<_>>().join(".")
+}
+
 #[rstest]
 /// Test that the Cargo.tomls do not have issues that make `cargo publish` hard
 fn test_cargo_toml_format(
@@ -67,4 +74,73 @@ fn test_cargo_toml_format(
             }
         }
     }
+
+    // Check that there is a consistent package.rust-version amongst all packages since proper
+    // MSRV support requires it.
+    let package = toml.get("package").and_then(|p| p.as_table());
+    let workspace = toml.get("workspace").and_then(|p| p.as_table());
+
+    if package.is_none() && workspace.is_none() {
+        panic!(
+            "{} is neither a package nor a workspace?",
+            toml_path.display()
+        );
+    }
+
+    if let Some(package) = package {
+        let rust_version = package
+            .get("rust-version")
+            .and_then(|v| v.as_str())
+            .unwrap_or_else(|| panic!("Missing package.rust-version in {}", toml_path.display()));
+
+        assert_eq!(
+            rust_version,
+            msrv_major_minor(),
+            "package.rust-version in {} must equal MSRV ({})",
+            toml_path.display(),
+            msrv_major_minor()
+        );
+    }
+}
+
+#[rstest]
+/// Check that the UI tests run on the MSRV
+fn test_msrv_ui(
+    // .. since workspace root is parent of package root
+    #[files("../metrique/tests/ui.rs")] rs_path: PathBuf,
+) {
+    let msrv_string = format!("stable({MSRV})");
+    let file = std::fs::read_to_string(rs_path).unwrap();
+    assert!(
+        file.contains("rustversion"),
+        "ui.rs does not contain rustversion, this test needs to be updated to the new mechanism"
+    );
+    for line in file.lines() {
+        if line.contains("rustversion") {
+            assert!(
+                line.contains(&msrv_string),
+                "version {} does not contain msrv {}",
+                line,
+                msrv_string
+            );
+        }
+    }
+}
+
+#[rstest]
+/// Check that build yml tests on the MSRV
+fn test_build_yml(
+    // .. since workspace root is parent of package root
+    #[files("../Cargo.toml")] base_path: PathBuf,
+) {
+    let rs_path = base_path
+        .parent()
+        .unwrap()
+        .join(".github/workflows/build.yml");
+    let msrv_string = format!("- \"{MSRV}\" # Current MSRV");
+    let file = std::fs::read_to_string(rs_path).unwrap();
+    assert!(
+        file.contains(&msrv_string),
+        "build.yml must run at the msrv"
+    );
 }
